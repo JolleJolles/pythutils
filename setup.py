c46b363f9c3b76c4041bd6ac07dca528693feda8
joljols <j.w.jolles@gmail.com>
2018-08-01 11:29:54 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+#
+# Copyright (C) 2018 Jolle Jolles <j.w.jolles@gmail.com>
+
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+def check_dependencies():
+    install_requires = []
+
+    try:
+        import numpy
+    except ImportError:
+        install_requires.append('numpy')
+
+    return install_requires
+
+install_requires = check_dependencies()
+
+setup(name='animlab',
+      author='Jolle Jolles',
+      author_email='j.w.jolles@gmail.com',
+      description='AnimLab: Sophisticated computational tools for studying animal behaviour',
+      long_description=readme,
+      url='http://jollejolles.com',
+      download_url='https://github.com/jolleslab/animlab.git',
+      version="0.0.1",
+      license="MIT",
+      install_requires=install_requires,
+      packages=find_packages(exclude=('tests', 'docs')))
