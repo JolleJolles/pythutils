95ebee594d1fcf8e81abfbf22eb4d94c73fd9fc9
joljols <j.w.jolles@gmail.com>
2018-08-02 14:20:22 +0200

    some additions and fixes

--- animlab/imutils.py
+++ animlab/imutils.py
@@ -3,8 +3,27 @@
 
 # In[ ]:
 
-
+import os
 import cv2
+from animlab.utils import *
+
+
+def check_media(mediafile):
+    
+    ext = get_ext(mediafile)
+    ftype = "vid" if ext in [".mov",".mp4",".avi"] else "img" if ext in [".jpg", ".png", ".jpeg", ".bmp"] else None
+    filedir = os.path.dirname(mediafile)
+
+    assert ftype != None, "File neither Video or image file.."    
+    assert os.path.isdir(filedir), "File directory does not exist.."    
+    assert os.path.isfile(mediafile), "File does not exist.."
+
+    if ftype == "vid":
+        cap = cv2.VideoCapture(mediafile)
+        assert cap.read()[0], "Video source opened but failed to read any images.." 
+    
+    print ftype, "file okay!",
+
 
 def getimg(mediafile):
                 
@@ -40,7 +59,7 @@ def crop(image, pt1, pt2):
     return cropped
 
 
-def newdims(img = None, dims = None, resize = 1):
+def newdims(img = None, resize = 1, dims = None):
     
     """ Returns new dimensions based on resize value"""
     
@@ -49,6 +68,7 @@ def newdims(img = None, dims = None, resize = 1):
             dims = (img.shape[1],img.shape[0])
         else:
             print "No img or dims provided.."
+            return
     
     width = int(dims[0] * resize)
     height = int(dims[1] * resize)
@@ -56,7 +76,7 @@ def newdims(img = None, dims = None, resize = 1):
     return (width, height)
 
 
-def imgresize(img, resize = 1, dims = None, back = False):                
+def imresize(img, resize = 1, dims = None, back = False):                
 
     """
     Returns resized image based on resizevalue or provided dims
@@ -80,4 +100,35 @@ def imgresize(img, resize = 1, dims = None, back = False):
     img = cv2.resize(img, dims, interpolation = interpol) 
 
     return img
+
+
+def textdims(text, fontsize, thickness = 1):
+    
+    tw, th = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, fontsize, thickness)[0]
+    
+    topy = fontsize*1 if any(x in ["i","j"] for x in text) else 0
+    boty = fontsize*2 if any(x in "Q" for x in text) else 0
+    boty = fontsize*7 if any(x in ["g","j","y","p","q"] for x in text) else boty
+    
+    return (tw, th), topy, boty
+
+
+def draw_text(img, text, loc = (0, 0), fontsize = 1, col = (0,0,0), margin = 5, bgcol = None):
+     
+    (tw, th), topy, boty = textdims(text, fontsize)
+    print (tw, th), topy, boty
+    print margin
+    
+    if bgcol is not None:
+        topleftout = (loc[0], loc[1])
+        botrightout = (loc[0]+margin+tw+margin, loc[1]+margin+th+topy+boty+margin)
+        cv2.rectangle(img, topleftout, botrightout, bgcol, -1)
+
+    botlefin = (loc[0]+margin, loc[1]+margin+th+topy)
+    cv2.putText(img, text, botlefin, cv2.FONT_HERSHEY_SIMPLEX, fontsize, col, 1, cv2.LINE_AA)
+    
+
+def draw_crosshair(img, x, y):
+    cv2.line(img, (x - 5, y), (x + 5, y), namedcols("whi"), 1)
+    cv2.line(img, (x, y - 5),( x, y + 5), namedcols("whi"), 1)
 
