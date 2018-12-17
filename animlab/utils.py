9c7a20912691b946c77dcfa4cc4a25edf87abb84
Jolle Jolles <jollejolles@Jolles-MBP.fritz.box>
2018-12-17 23:14:20 +0100

    sys

--- animlab/utils.py
+++ animlab/utils.py
@@ -20,6 +20,7 @@ from __future__ import print_function
 
 import datetime
 import os
+import sys
 import time
 import numpy as np
 import pandas as pd
@@ -39,7 +40,6 @@ class Logger(object):
         self.log.write(message)
     def flush(self):
         pass
-
 
 def deleteline(n=1):
 
