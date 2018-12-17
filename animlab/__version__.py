d7f7ff7d1e86286aa29f1e7f883188e18a14a524
joljols <j.w.jolles@gmail.com>
2018-12-17 13:29:03 +0100

    lineprint

--- animlab/__version__.py
+++ animlab/__version__.py
@@ -3,5 +3,6 @@
 __version__ = "1.0.1"
 
 #17/12/18 - 1.0.1  Changes:
+# - Updated lineprint function to show new lines when run as single command
 # - Created version document that is automatically called when installing animlab
 # - Removed automatic loading of imutils for users that do not have opencv installed
