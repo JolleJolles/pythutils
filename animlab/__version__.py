4aa1f6d007b513308507806054c690aef939e9bc
joljols <j.w.jolles@gmail.com>
2018-12-17 14:23:15 +0100

    logger

--- animlab/__version__.py
+++ animlab/__version__.py
@@ -3,6 +3,7 @@
 __version__ = "1.0.1"
 
 #17/12/18 - 1.0.1  Changes:
+# - Added logger class
 # - Updated lineprint function to show new lines when run as single command
 # - Created version document that is automatically called when installing animlab
 # - Removed automatic loading of imutils for users that do not have opencv installed
