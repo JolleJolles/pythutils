f1a504622545c4610244cddd32dc4934fea22558
Jolle Jolles <jollejolles@Jolles-MacBook-Pro-3.local>
2019-06-18 16:39:31 +0200

    1.4.4

--- animlab/utils.py
+++ animlab/utils.py
@@ -19,6 +19,7 @@ from __future__ import division
 from __future__ import print_function
 
 import re
+import io
 import os
 import sys
 import time
@@ -90,6 +91,32 @@ def clock():
     while True:
         print(datetime.datetime.now().strftime("%H:%M:%S")+"\r")
         time.sleep(1)
+
+
+def is_rpi():
+
+    """ Checks if current system is a Raspberry Pi """
+
+    try:
+        with io.open('/proc/cpuinfo', 'r') as cpuinfo:
+            found = False
+            for line in cpuinfo:
+                if line.startswith('Hardware'):
+                    found = True
+                    label, value = line.strip().split(':', 1)
+                    value = value.strip()
+                    if value not in ('BCM2708','BCM2709','BCM2835','BCM2836'):
+                        return False
+            if not found:
+                raise ValueError('Unable to determine if system is rpi or\n
+                      not. Set system manually')
+                else:
+                    return False
+
+    except IOError:
+        return False
+
+    return True
 
 
 def isscript():
