e96b0ab024e0edcb934acda094fe8b219d86e4c0
joljols <j.w.jolles@gmail.com>
2019-11-06 15:11:18 +0100

    updated version

--- pythutils/__version__.py
+++ pythutils/__version__.py
@@ -13,6 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "1.4.5"
+__version__ = "1.0.0"
 
 # For documentation of all changes, see CHANGELOG
