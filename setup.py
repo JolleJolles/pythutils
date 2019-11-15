b2dd0cf7fa894d912b178a1da49de6618a93ed83
Jolle Jolles <j.w.jolles@gmail.com>
2019-11-15 16:20:07 +0100

    dependencies fix

--- setup.py
+++ setup.py
@@ -16,46 +16,47 @@
 from setuptools import setup, find_packages
 import sys
 
-exec(open('pythutils/__version__.py').read())
+exec(open("pythutils/__version__.py").read())
 
 DESCRIPTION = """pythutils: a collection of utility functions for Python"""
 
-DISTNAME = 'pythutils'
-MAINTAINER = 'Jolle Jolles'
-MAINTAINER_EMAIL = 'j.w.jolles@gmail.com'
-URL = 'https://github.com/JolleJolles'
-DOWNLOAD_URL = 'https://github.com/JolleJolles/pythutils/archive/1.1.0.tar.gz'
-LICENSE = 'Apache Software License 2.0'
+DISTNAME = "pythutils"
+MAINTAINER = "Jolle Jolles"
+MAINTAINER_EMAIL = "j.w.jolles@gmail.com"
+URL = "https://github.com/JolleJolles"
+DOWNLOAD_URL = "https://github.com/JolleJolles/pythutils/archive/1.1.0.tar.gz"
+LICENSE = "Apache Software License 2.0"
 
 
-with open('README.md') as f:
+with open("README.md") as f:
     readme = f.read()
 
 def check_dependencies():
+
     install_requires = []
 
     try:
         import numpy
     except ImportError:
         if sys.version_info[0] == 2:
-            install_requires.append('numpy==1.16.5')
+            install_requires.append("numpy==1.16.5")
         if sys.version_info[0] == 3:
-            install_requires.append('numpy')
+            install_requires.append("numpy")
     try:
         import pandas
     except ImportError:
         if sys.version_info[0] == 2:
-            install_requires.append('pandas==0.24.2')
+            install_requires.append("pandas==0.24.2")
         if sys.version_info[0] == 3:
-            install_requires.append('pandas')
+            install_requires.append("pandas")
     try:
         import yaml
     except ImportError:
-        install_requires.append('pyyaml')
+        install_requires.append("pyyaml")
     try:
         import h5py
     except ImportError:
-        install_requires.append('h5py')
+        install_requires.append("h5py")
 
     return install_requires
 
@@ -76,17 +77,17 @@ if __name__ == "__main__":
           download_url=DOWNLOAD_URL,
           version=__version__,
           install_requires=install_requires,
-          packages=['pythutils'],
+          packages=["pythutils"],
           classifiers=[
-                     'Intended Audience :: Science/Research',
-                     'Programming Language :: Python :: 2.7',
-                     'Programming Language :: Python :: 3',
-                     'License :: OSI Approved :: Apache Software License',
-                     'Topic :: Scientific/Engineering :: Visualization',
-                     'Topic :: Scientific/Engineering :: Image Recognition',
-                     'Topic :: Scientific/Engineering :: Information Analysis',
-                     'Topic :: Multimedia :: Video',
-                     'Operating System :: POSIX',
-                     'Operating System :: Unix',
-                     'Operating System :: MacOS'],
+                     "Intended Audience :: Science/Research",
+                     "Programming Language :: Python :: 2.7",
+                     "Programming Language :: Python :: 3",
+                     "License :: OSI Approved :: Apache Software License",
+                     "Topic :: Scientific/Engineering :: Visualization",
+                     "Topic :: Scientific/Engineering :: Image Recognition",
+                     "Topic :: Scientific/Engineering :: Information Analysis",
+                     "Topic :: Multimedia :: Video",
+                     "Operating System :: POSIX",
+                     "Operating System :: Unix",
+                     "Operating System :: MacOS"],
           )
