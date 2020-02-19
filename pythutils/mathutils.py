c1a32790e34130454458e4d60414a00b42ce1217
joljols <j.w.jolles@gmail.com>
2020-02-19 09:13:53 +0100

    fixed varname in seqcount

--- pythutils/mathutils.py
+++ pythutils/mathutils.py
@@ -53,7 +53,7 @@ def seqcount(start, stop, steplength):
     certain steplength
     """
 
-    step = (stop - start) / length
+    step = (stop - start) / steplength
     step = int(np.ceil(step))
     sequence = range(start, stop, step)
 
