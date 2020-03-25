dde3f32df26e2d659a5fd9713c60384d89a2bbbd
Jolle Jolles <j.w.jolles@gmail.com>
2020-03-25 17:41:09 +0100

    added fourpt_transform function

--- pythutils/mediautils.py
+++ pythutils/mediautils.py
@@ -136,6 +136,34 @@ def crop(image, pt1, pt2):
     return cropped
 
 
+def fourpt_transform(image, pts):
+
+    """
+    Perspective transform a section of an image based on four coordinates
+    to obtain a top-down view
+    """
+
+    rect = order_points(pts)
+    (tl, tr, br, bl) = rect
+
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+
+    dst = np.array([[0, 0], [maxWidth - 1, 0],
+                    [maxWidth - 1, maxHeight - 1],
+                    [0, maxHeight - 1]], dtype = "float32")
+
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+
+    return warped
+
+
 def checkroi(roi, resolution):
 
     """Make sure roi coordinates are within resolution"""
